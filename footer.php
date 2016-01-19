95d03cd9314ccf420fa5a0f61ef23099ad6b25b0
Tung Pham <tjntun@gmail.com>
2016-01-20 02:06:41 +0700

    finish home, add contact page

---
+++ footer.php
@@ -0,0 +1,44 @@
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <ul id="footer-menu">
+                    <li><a class="active" href="#">home</a></li>
+                    <li><a href="#">contact</a></li>
+                    <li><a href="#">account</a></li>
+                    <li><a href="#">post ad</a></li>
+                    <li><a href="#">find growers</a></li>
+                    <li><a href="#">faq</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <!-- popups -->
+    <section id="legal-page">
+        <div class="content fluid-center">
+            <h2 class="title">Age Restricted Content</h2>
+            <p>Marijuana Exchange does business in accordance 
+            with state laws regarding access to cannabis.
+            <br>
+            You must be at least 21 years old, or a
+            valid medical marijuana patient.
+            <br>
+            Are you eligible to visit
+            Marijuana Exchange?</p>
+            <div class="btn-group">
+                <div class="btn bg-cover no">no</div>
+                <div class="btn bg-cover yes">yes</div>
+            </div>
+        </div>
+    </section>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+    </script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
