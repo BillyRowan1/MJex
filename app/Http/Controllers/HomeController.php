fb57bb93343699e86e9073548964d2bb5b461af3
Tung Pham <tjntun@gmail.com>
2016-02-15 01:36:20 +0700

    remove auth on homepage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,6 @@ use Mjex\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
